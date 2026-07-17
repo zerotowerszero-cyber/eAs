f533a6faa7e49279cac77cb0c54f7d2ee8f1b165
Your Name <zerotowerszero@gmail.com>
2026-07-16 21:42:55 -0400

    fix: syntax error in VideoPlayer component

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -149,6 +149,7 @@ export default function VideoPlayer({ type, tmdbId, season, episode }: VideoPlay
             allow="autoplay; encrypted-media"
           />
         )}
+      </div>
         
       {/* Controls Below Player */}
       <div style={{ marginTop: "32px" }}>
