8540b636126f2fdb8d3389c4d0a36c2eb334a24d
Your Name <zerotowerszero@gmail.com>
2026-07-16 21:29:02 -0400

    ui: force instant favicon reload when system theme changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,20 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Product+Sans&amp;family=Google+Sans+Display:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;family=Google+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;family=Google+Sans+Text:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;family=Google+Sans+Mono:wght@400;500;700&amp;family=Roboto+Mono:wght@400;500;700&amp;display=swap" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function() {
+              const matcher = window.matchMedia('(prefers-color-scheme: dark)');
+              matcher.addEventListener('change', () => {
+                const icon = document.querySelector('link[rel="icon"]');
+                if (icon) {
+                  const currentHref = icon.href.split('?')[0];
+                  icon.href = currentHref + '?v=' + Date.now();
+                }
+              });
+            })();
+          `
+        }} />
       </head>
       <body>{children}</body>
     </html>
